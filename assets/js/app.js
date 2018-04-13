3403980bab0818d777d97355cb2f251e5f95b0f4
Iz P <pehisaac@gmail.com>
2018-04-12 19:07:14 -0600

    Some progress.

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,8 +60,8 @@ $("#btnLogOut").on("click", function () {
 
 //creating a function to build new timers to the page
 //TODO - build in functionality for a key, so that this object can be referenced by the key in teh database object.
-function newTimer (name) {
-  
+function newTimer(name) {
+
   // pieces of the timer object
   var timeObject = $('<div>');
   var timeName = $('<p>' + name + '</p>');
@@ -100,19 +100,19 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
     $("#userName").text("Hi " + firebaseUser.email + "!");
 
     // listener for data
-// firebase.database().ref('/users/' + myID).on('value', function (snapshot) {
-//   console.log(snapshot.val() + " wizard");
-// });
+    // firebase.database().ref('/users/' + myID).on('value', function (snapshot) {
+    //   console.log(snapshot.val() + " wizard");
+    // });
 
 
-// TODO - this is pulling data from teh database just fine. for every project that is in the database we need to load a new timer in the paused status and append it to the screen. ordered by date added is just fine
+    // TODO - this is pulling data from teh database just fine. for every project that is in the database we need to load a new timer in the paused status and append it to the screen. ordered by date added is just fine
 
-db.ref('/users/' + myID).orderByChild("dateAdded").on("child_added", function(snapshot) {
-  var sv = snapshot.val();
+    db.ref('/users/' + myID).orderByChild("dateAdded").on("child_added", function (snapshot) {
+      var sv = snapshot.val();
 
-  console.log(sv.dateAdded + `date`);
-  console.log(sv.projectName + ` name`);
-})
+      console.log(sv.dateAdded + `date`);
+      console.log(sv.projectName + ` name`);
+    })
 
 
 
@@ -126,7 +126,8 @@ db.ref('/users/' + myID).orderByChild("dateAdded").on("child_added", function(sn
 
   } else {
     console.log('not logged in');
-    console.log(firebaseUser)
+    firebaseTest();
+    // console.log(firebaseUser)
     logOut.classList.add("hide");
     $("#userName").html("<a href='sign-in.html'>Hi! Click to Log In</a>");
   }
@@ -135,66 +136,137 @@ db.ref('/users/' + myID).orderByChild("dateAdded").on("child_added", function(sn
 
 // **************************** END USER AUTHENTICATION ********************************
 
-$("#timer").on("click", function() {
+$("#timer").on("click", function () {
   $(`#timer`).css('border', 'green 5px solid')
 })
 
+
+function firebaseTest() {
+
+  //first thing is to write data
+
+  // update it by referencing the key
+  var newPostKey = firebase.database().ref().push().key;
+
+  var postData = {
+    time: 0,
+    projectName: `Jimmy's Timer`,
+    key: newPostKey,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  };
+
+
+  // this test will be called after the auth checks if there is a user
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['/timers/' + newPostKey] = postData;
+  updates['/user-timers/' + myID + '/' + newPostKey] = postData;
+
+  firebase.database().ref().update(updates);
+
+
+  //secondly i'm going to need to read from the database
+
+// create listener to catch changes in the database
+db.ref('/user-timers/' + myID + '/').orderByChild('dateAdded').on("child_added", function(snapshot) {
+  // Variable to store response from database
+  var sv = snapshot.val();
+
+  console.log(JSON.stringify(sv.key) + 'reading database test');
+  // console.log(sv.key + ' key');
+  
+
+
+});
+
+
+
+
+
+  //thirdly i need to be able to update and point to a particular timer object
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Modal Button Functionality for adding Project
-$("#projectSubmit").on('click', function(){
+$("#projectSubmit").on('click', function () {
   event.preventDefault();
   // capture input from  user
   let projectName = $('#projectName').val().trim();
-  
+
 
 
   // if (user === null || user == undefined){
   //   console.log("noone is logged in to store data for");
   // } else {
- 
-    var newPostKey = firebase.database().ref().child(myID).push().key;
+
+
   console.log(newPostKey);
 
- // a good example of pushing data to the database  
- db.ref("users/" + myID).push({
-   projectName: projectName,
-   time: 0,
-   key: newPostKey,
-   dateAdded: firebase.database.ServerValue.TIMESTAMP
- })
- 
-    // 04-11-18 next thing i need to be able to do is to update this data and to be able to filter current time objects by key
- 
+  // a good example of pushing data to the database  
+  db.ref("users/" + myID).push({
+    projectName: projectName,
+    time: 0,
+    key: newPostKey,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  })
 
-    var updates = {};
-    updates['/users/' + myID + "/-L9r-OpNLpxTDuaVTGh0" + "/time"] = 19;
-    // updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-
-    firebase.database().ref().update(updates);
-
-
-// how to find specific timer object
-
-
-// currently my database is built like this. i have a pool of data that i keep just pushing new objects into, which can be organized by most recent. however this becomes inconvenient when trying to update information because it's hard to point at objects.
-
-// to fix this i'm going to create a section for timers like this
-
-// users/timers/
-
-// then when a new timer is created it will set data for that like this
-
-// users/timers/key-token
-
-// then inside of that there will be the json object which contains all o the goodies.
-
-// this will then make available the option to save the key-token to the html object when it is created, and then when the timer is clicked or whatever we can reference the corect timer object in the database.
+  // 04-11-18 next thing i need to be able to do is to update this data and to be able to filter current time objects by key
 
 
 
 
 
 
- 
+
+  // var updates = {};
+  // updates['/users/' + myID + "/-L9r-OpNLpxTDuaVTGh0" + "/time"] = 19;
+  // // updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+  // firebase.database().ref().update(updates);
+
+
+  // how to find specific timer object
+
+
+  // currently my database is built like this. i have a pool of data that i keep just pushing new objects into, which can be organized by most recent. however this becomes inconvenient when trying to update information because it's hard to point at objects.
+
+  // to fix this i'm going to create a section for timers like this
+
+  // users/timers/
+
+  // then when a new timer is created it will set data for that like this
+
+  // users/timers/key-token
+
+  // then inside of that there will be the json object which contains all o the goodies.
+
+  // this will then make available the option to save the key-token to the html object when it is created, and then when the timer is clicked or whatever we can reference the corect timer object in the database.
+
+
+
+
+
+
+
   // }
 
   // setting timer variables to firebase server
@@ -204,7 +276,7 @@ $("#projectSubmit").on('click', function(){
   // time will be handled in stopwatch object
   // one time pull of totaltime
   // time will push new objects every day
-  
+
 
   // db.ref("users/" + myID).push({
   //   projectName: projectName,
@@ -217,23 +289,23 @@ $("#projectSubmit").on('click', function(){
 
 // playing with firebase database handling
 
-$("#accountSubmit").on("click", function (){
+$("#accountSubmit").on("click", function () {
 
-//  if (user === null || user == undefined){
-//    console.log("noone is logged in to store data for");
-//  } else {
-
-  
-// db.ref("users/" + myID).push({
-//   projectName: projectName,
-//   dateAdded: firebase.database.ServerValue.TIMESTAMP
-// })
+  //  if (user === null || user == undefined){
+  //    console.log("noone is logged in to store data for");
+  //  } else {
 
 
+  // db.ref("users/" + myID).push({
+  //   projectName: projectName,
+  //   dateAdded: firebase.database.ServerValue.TIMESTAMP
+  // })
 
 
-//  }
-  
+
+
+  //  }
+
 
 
 })
@@ -243,9 +315,9 @@ $("#accountSubmit").on("click", function (){
 //var userId = firebase.auth().currentmyID;
 // set data
 //firebase.database().ref('users/' + userId).set({
-    //username: "name",
-    //email: "email",
-    //profile_picture: "imageUrl"
+//username: "name",
+//email: "email",
+//profile_picture: "imageUrl"
 //});
 
 
@@ -258,7 +330,7 @@ $("#accountSubmit").on("click", function (){
 
 // listener for data
 //firebase.database().ref('/users/' + userId + '/email').on('value', function (snapshot) {
-  //  console.log(snapshot.val() + " wizard");
+//  console.log(snapshot.val() + " wizard");
 //});
 
 
@@ -372,12 +444,12 @@ var stopwatch = {
     clockRunning = false;
   },
   count: function () {
-    
-      stopwatch.time++;
-      // TODO - assign time to firebase database for network storage.
-      var var2 = stopwatch.timeConverter(stopwatch.time);
-      $('#time').text(var2);
-    
+
+    stopwatch.time++;
+    // TODO - assign time to firebase database for network storage.
+    var var2 = stopwatch.timeConverter(stopwatch.time);
+    $('#time').text(var2);
+
   },
   timeConverter: function (t) {
 
@@ -385,7 +457,7 @@ var stopwatch = {
     let hours = Math.floor(t / 3600);
     let newNumber = t - (hours * 60 * 60);
     var minutes = Math.floor(newNumber / 60);
-    
+
     var seconds = newNumber - (minutes * 60);
 
     if (seconds < 10) {
@@ -396,7 +468,7 @@ var stopwatch = {
       minutes = "00";
     }
 
-        
+
     else if (minutes < 10) {
       minutes = "0" + minutes;
     }
@@ -409,7 +481,7 @@ var stopwatch = {
       hours = "0" + hours;
     }
     console.log(seconds, minutes, hours);
-    
+
     return hours + `:` + minutes + ":" + seconds;
   }
 };
