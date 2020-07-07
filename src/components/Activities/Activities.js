f78c3981a387ce3cabfd113182fe922e69a68bd5
Izzle <pehisaac@gmail.com>
2020-07-06 21:06:17 -0600

    activites cmpnt

---
+++ src/components/Activities/Activities.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Activities.scss'
+
+export default function Activities() {
+    return (
+        <section class="activities">
+            <div class="activity-copy">
+                <p class="title">Activities</p>
+            </div>
+            <div class="activity-buttons">
+                <button class="activity">Break</button>
+                <button class="activity">Work</button>
+                <button class="activity">Lunch</button>
+                <button class="activity">Team Coding</button>
+                <button class="activity">Personal Coding</button>
+                <button class="activity">Lunch</button>
+                <button class="activity">Coding</button>
+                <button class="activity">Coding</button>
+            </div>
+        </section>
+    )
+}
